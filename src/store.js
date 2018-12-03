a4e38bfd638f1b7b606d9fa6cb0241d59ca959ba
Krystian Kaminski <krystiankaminski07@gmail.com>
2018-12-03 10:36:30 +0100

    Reducer for ADD_TODO

--- src/store.js
+++ src/store.js
@@ -1,8 +1,21 @@
+const ADD_TODO = 'ADD_TODO'
+
+const addTodo = text => ({
+    type: ADD_TODO,
+    text
+})
+
+
 export default (state = [], action) => {
 
-    switch(action.type) {
-        case 'ADD_TODO':
-            return state
+    switch (action.type) {
+        case ADD_TODO:
+            const newTodo = {
+                text: action.text,
+                completed: false
+
+            }
+            return [...state, newTodo]
 
         default:
             return state
