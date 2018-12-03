3cd70453d1923d8810e85dcf4c09de8767da0d52
Krystian Kaminski <krystiankaminski07@gmail.com>
2018-12-03 13:26:59 +0100

    Improve addToCart reducer part 2

--- src/store/cart.js
+++ src/store/cart.js
@@ -2,6 +2,11 @@
 
 const ADD_CART = 'ADD_CART'
 
+const INITIAL_STATE = {
+    cartTotal: 0,
+    items: []
+}
+
 
 export const addToCart = (title, price) => ({
     type: ADD_CART,
@@ -17,7 +22,11 @@ export default (state = [], action) => {
 
     switch(action.type) {
         case ADD_CART:
-           return [...state, action.item]
+           return{
+               ...state,
+               items: [...state.items, action.item],
+               cartTotal: (state.cartTotal + action.item.price)
+           }
         default:
             return state
     }
