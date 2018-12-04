17a0ea8de188cb942349f24bdcc9696b1edd8244
Krystian Kaminski <krystiankaminski07@gmail.com>
2018-12-04 11:08:15 +0100

    Connect action toggleTodo

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import { connect } from 'react-redux'
 
-import { addTodo } from './store/todos'
+import { addTodo, toggleTodo } from './store/todos'
 
 const mapStateToProps = store => ({
     _todos: store.todos.allTodos
 })
 
 const mapDispatchToProps = dispatch => ({
-    _addTodo: text => dispatch(addTodo(text))
+    _addTodo: text => dispatch(addTodo(text)),
+    _toggleTodo: index => dispatch(toggleTodo(index))
 })
 
 class TodoList extends React.Component {
@@ -17,23 +18,35 @@ class TodoList extends React.Component {
     state = { value: '' }
 
     handleInputChange = event => {
-        this.setState({value: event.target.value})
+        this.setState({ value: event.target.value })
     }
 
     handleButtonClick = () => {
         console.log('Want to save todo: ', this.state.value)
         this.props._addTodo(this.state.value)
     }
+
+    handleTodoClick = (index) => {
+        this.props._toggleTodo(index)
+    }
+
     render() {
         console.log('TodoList props', this.props)
         return <div>
-            <input onChange={this.handleInputChange}/>
+            <input onChange={this.handleInputChange} />
             <button onClick={this.handleButtonClick}>Add todo</button>
-            {this.props._todos.map(todo =>
-                <div key={todo.text}>{todo.text}</div>
+            {this.props._todos.map((todo, index) =>
+                <div
+                    style={{
+                        textDecoration: todo.completed ? 'line-through' : 'none'
+                    }}
+                    onClick={() => this.handleTodoClick(index)}
+                    key={todo.text}>
+                    {todo.text}
+                </div>
             )}
         </div>
-        
+
     }
 }
 
