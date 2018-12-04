f81fb9afa156908b4ae27647e6f25869b7e52b19
Krystian Kaminski <krystiankaminski07@gmail.com>
2018-12-04 09:29:51 +0100

    Added Provider from react-redux

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore, combineReducers } from 'redux'
+import { Provider } from 'react-redux'
 import './index.css';
 import App from './App';
 
 // REDUCERS IMPORT
 import todos, { addTodo, filterTodos, toggleTodo, deleteTodo } from './store/todos'
-import counter, {increment, decrement, reset} from './store/counter'
-import cart, {addToCart} from './store/cart'
+import counter, { increment, decrement, reset } from './store/counter'
+import cart, { addToCart } from './store/cart'
 
 // REDUX CONFIG GOES HERE
 
@@ -28,18 +29,8 @@ const store = createStore(
 
 
 
-
-
-window.increaseCounter = () => store.dispatch(increment())
-window.decreaseCounter = () => store.dispatch(decrement())
-window.reset = () => store.dispatch(reset())
-window.addToCart = (title, price) => store.dispatch(addToCart(title, price))
-
-
-window.addTodo = (text) => store.dispatch(addTodo(text))
-window.filterTodos = (text) => store.dispatch(filterTodos(text))
-window.toggleTodo = index => store.dispatch(toggleTodo(index))
-window.deleteTodo = index => store.dispatch(deleteTodo(index))
-
-
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+    , document.getElementById('root'));
