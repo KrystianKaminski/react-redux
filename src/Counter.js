d0242285a95fc83d0304a9f4a56e88e986f45f44
Krystian Kaminski <krystiankaminski07@gmail.com>
2018-12-04 14:36:03 +0100

    Another method - reset to 0

--- src/Counter.js
+++ src/Counter.js
@@ -1,22 +1,24 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import { increment,decrement } from './store/counter'
+import { increment, decrement, reset } from './store/counter'
 
 const mapStateToProps = store => ({
     _counterValue: store.counter
 })
 
 const mapDispatchToProps = dispatch => ({
-    _increment: () => dispatch(increment()), 
-    _decrement: () => dispatch(decrement()) 
+    _increment: () => dispatch(increment()),
+    _decrement: () => dispatch(decrement()),
+    _reset: () => dispatch(reset()),
 })
 
-const Counter =  props => {
+const Counter = props => {
     console.log('Counter props', props)
     return <div>
         <div>Count state: {props._counterValue}</div>
         <button onClick={() => props._increment()}>+</button>
         <button onClick={() => props._decrement()}>-</button>
+        <button onClick={() => props._reset()}>RESET TO 0</button>
     </div>
 }
 
